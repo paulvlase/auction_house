33671d1640d64c2c49442b0629c7c8bc45d10912
Paul Vlase <vlase.paul@gmail.com>
2013-03-17 20:04:08 +0200

    config: Added Filesconfig.

---
+++ AuctionHouse/src/config/FilesConfig.java
@@ -0,0 +1,8 @@
+package config;
+
+public class FilesConfig {
+	//public static String LOGIN_FILENAME = "invalidLogin.conf"; 
+	public static String LOGIN_FILENAME = "login.conf";
+	public static String DEMANDS_FILENAME = "demands.conf";
+	public static String SUPPLIES_FILENAME = "supplies.conf";
+}
