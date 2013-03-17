f6798aa177409dff263a1379d4a9376eef67f600
Ghennadi Procopciuc <unix140@gmail.com>
2013-03-17 16:32:14 +0200

    app: Sets look and feel

--- AuctionHouse/src/app/Main.java
+++ AuctionHouse/src/app/Main.java
@@ -1,5 +1,8 @@
 package app;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import webServiceClient.WebServiceClientImpl;
 import network.NetworkImpl;
 import gui.GuiImpl;
@@ -7,6 +10,15 @@ import mediator.MediatorImpl;
 
 public class Main {
 	public static void main(String[] args) {
+		// TODO : Check if this look and feel is available on system
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (Exception e) {
+			try {
+				UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+			} catch (Exception e1) {}
+		}
+		
 		MediatorImpl med = new MediatorImpl();
 		GuiImpl gui = new GuiImpl(med);
 		NetworkImpl net = new NetworkImpl(med);
