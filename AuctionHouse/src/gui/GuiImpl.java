e3237ed6f807b3f666f4f79488a2f2b6ecbe6955
Paul Vlase <vlase.paul@gmail.com>
2013-03-17 15:10:21 +0200

    gui: MediatorGui and Login references

--- AuctionHouse/src/gui/GuiImpl.java
+++ AuctionHouse/src/gui/GuiImpl.java
@@ -1,12 +1,27 @@
 package gui;
 
 import interfaces.Gui;
+import interfaces.MediatorGui;
 
 /**
+ * Gui module implementation.
  * 
  * @author Paul Vlase <vlase.paul@gmail.com>
- * 
  */
-public class GuiImpl implements Gui  {
+public class GuiImpl implements Gui {
+	private MediatorGui	med;
+	private Login		login;
 
+	public GuiImpl(MediatorGui med) {
+		this.med = med;
+
+		med.registerGui(this);
+		
+		login = new Login(this);
+		login.setVisible(true);
+	}
+
+	public String getName() {
+		return this.med.getName();
+	}
 }
