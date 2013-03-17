8375f74a0f70df01083582417406ebcc51c6fa00
Paul Vlase <vlase.paul@gmail.com>
2013-03-17 15:39:16 +0200

    mediator: More changes.

--- AuctionHouse/src/mediator/MediatorImpl.java
+++ AuctionHouse/src/mediator/MediatorImpl.java
@@ -1,9 +1,11 @@
 package mediator;
 
+import interfaces.Gui;
 import interfaces.MediatorGui;
 import interfaces.MediatorNetwork;
 import interfaces.MediatorWeb;
-
+import interfaces.Network;
+import interfaces.WebServiceClient;
 
 /**
  * Mediator module implementation.
@@ -11,8 +13,33 @@ import interfaces.MediatorWeb;
  * @author Paul Vlase <vlase.paul@gmail.com>
  */
 public class MediatorImpl implements MediatorGui, MediatorNetwork, MediatorWeb {
-	
+	private Gui					gui;
+	private Network				net;
+	private WebServiceClient	web;
+
+	private String				name;
+
 	public MediatorImpl() {
-		
+
+	}
+
+	public void registerGui(Gui gui) {
+		this.gui = gui;
+	}
+
+	public void registerNetwork(Network net) {
+		this.net = net;
+	}
+
+	public void registerWebServiceClient(WebServiceClient web) {
+		this.web = web;
+	}
+	
+	public boolean signIn(String username, String password) {
+		return web.signIn(username, password);
+	}
+
+	public String getName() {
+		return this.name;
 	}
 }
