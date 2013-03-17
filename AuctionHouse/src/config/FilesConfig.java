067a8936e3c154d15a4c9b4077e6f5edae151d19
Paul Vlase <vlase.paul@gmail.com>
2013-03-18 00:08:15 +0200

    config: Filename refactoring.

--- AuctionHouse/src/config/FilesConfig.java
+++ AuctionHouse/src/config/FilesConfig.java
@@ -3,6 +3,6 @@ package config;
 public class FilesConfig {
 	//public static String LOGIN_FILENAME = "invalidLogin.conf"; 
 	public static String LOGIN_FILENAME = "login.conf";
-	public static String DEMANDS_FILENAME = "demands.conf";
-	public static String SUPPLIES_FILENAME = "supplies.conf";
+	public static String DEMANDS_FILENAME = "demands.list";
+	public static String SUPPLIES_FILENAME = "supplies.list";
 }
