872b9410aa8418a50cb40e1a90b9b2a1a372b25e
Paul Vlase <vlase.paul@gmail.com>
2013-03-17 15:37:44 +0200

    webServiceClient: More changes.

--- AuctionHouse/src/webServiceClient/WebServiceClientImpl.java
+++ AuctionHouse/src/webServiceClient/WebServiceClientImpl.java
@@ -1,10 +1,26 @@
 package webServiceClient;
 
+import interfaces.MediatorWeb;
+import interfaces.WebServiceClient;
+
 /**
  * WebServiceClient module implementation.
  * 
  * @author Paul Vlase <vlase.paul@gmail.com>
  */
-public class WebServiceClientImpl {
-
+public class WebServiceClientImpl implements WebServiceClient {
+	private MediatorWeb med = null;
+	
+	public WebServiceClientImpl(MediatorWeb med) {
+		this.med = med;
+		
+		med.registerWebServiceClient(this);
+	}
+	
+	public boolean signIn(String username, String password) {
+		if (username == "pvlase" && password == "parola")
+			return true;
+		
+		return false;
+	}
 }
