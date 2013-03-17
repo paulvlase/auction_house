25d8391ed8120612868b325adc122df2f81b4ae3
Paul Vlase <vlase.paul@gmail.com>
2013-03-17 20:02:43 +0200

    webServiceClient: add new user.

--- AuctionHouse/src/webServiceClient/WebServiceClientImpl.java
+++ AuctionHouse/src/webServiceClient/WebServiceClientImpl.java
@@ -17,11 +17,65 @@ public class WebServiceClientImpl implements WebServiceClient {
 		med.registerWebServiceClient(this);
 	}
 	
+	@Override
 	public boolean signIn(String username, String password) {
 		System.out.println("[WebServiceClientImpl : signIn] " + username + " " + password);
 		if (username.equals("pvlase") && password.equals("parola"))
 			return true;
+		if (username.equals("unix140") && password.equals("marmota"))
+			return true;
 		
 		return false;
+	}
+
+	/* Common */
+	@Override
+	public int addOffer(String service) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int removeOffer(String service) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	@Override
+	public int launchOffer(String service) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int dropOffer(String service) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* Buyer */
+	@Override
+	public int acceptOffer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int refuseOffer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* Seller */
+	@Override
+	public int makeOffer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int dropAction() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
