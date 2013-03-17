e0e22919301cc004400478ad274a88d4cf046071
Paul Vlase <vlase.paul@gmail.com>
2013-03-17 13:48:57 +0200

    mediator: Initial sketch.

---
+++ AuctionHouse/src/mediator/MediatorImpl.java
@@ -0,0 +1,18 @@
+package mediator;
+
+import interfaces.MediatorGui;
+import interfaces.MediatorNetwork;
+import interfaces.MediatorWeb;
+
+
+/**
+ * Mediator module implementation.
+ * 
+ * @author Paul Vlase <vlase.paul@gmail.com>
+ */
+public class MediatorImpl implements MediatorGui, MediatorNetwork, MediatorWeb {
+	
+	public MediatorImpl() {
+		
+	}
+}
