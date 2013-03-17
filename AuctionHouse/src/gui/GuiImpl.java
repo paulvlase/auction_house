bffcb45f2ea10b124658339cc4c4c0b10d2569b5
Paul Vlase <vlase.paul@gmail.com>
2013-03-17 15:39:47 +0200

    gui: Added signIn method.

--- AuctionHouse/src/gui/GuiImpl.java
+++ AuctionHouse/src/gui/GuiImpl.java
@@ -1,5 +1,6 @@
 package gui;
 
+import config.GlobalConfig.UserType;
 import interfaces.Gui;
 import interfaces.MediatorGui;
 
@@ -23,5 +24,18 @@ public class GuiImpl implements Gui {
 
 	public String getName() {
 		return this.med.getName();
+	}
+	
+	public void signIn(String username, String password, UserType type) {
+		if (med.signIn(username, password)) {
+			login.setVisible(false);
+			
+			//TODO: autentificare reusita, afisez fereastra pentru
+			// cumparator, vanzator
+			System.out.println("Signed in");
+		} else {
+			// autentificare esuata, afisare dialog
+			System.out.println("Wrong username or password");
+		}
 	}
 }
