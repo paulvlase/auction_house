f3d84a518034364cbc07b874ad24fbaf798aba3c
Paul Vlase <vlase.paul@gmail.com>
2013-03-17 13:31:32 +0200

    network: Initial sketch.

---
+++ AuctionHouse/src/network/NetworkImpl.java
@@ -0,0 +1,12 @@
+package network;
+
+import interfaces.Network;
+
+/**
+ * Network module implementation.
+ * 
+ * @author Paul Vlase <vlase.paul@gmail.com>
+ */
+public class NetworkImpl implements Network {
+
+}
