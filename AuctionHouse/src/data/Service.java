808d2ce59b37627b62bbdf7840c79f3a525f3322
Ghennadi Procopciuc <unix140@gmail.com>
2013-03-20 21:01:26 +0200

    data: More methods

--- AuctionHouse/src/data/Service.java
+++ AuctionHouse/src/data/Service.java
@@ -1,35 +1,39 @@
 package data;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.swing.JProgressBar;
 
 /**
- * If user field is null, then this offer is inactive, otherwise it's active.
- * If time is 0, this offer is expired.
- * If price is 0, this offer is a demand, otherwise it's supply.
+ * If user field is null, then this offer is inactive, otherwise it's active. If
+ * time is 0, this offer is expired. If price is 0, this offer is a demand,
+ * otherwise it's supply.
+ * 
  * @author Ghennadi Procopciuc
  */
 public class Service {
-	private String name;
-	private long time;
-	private double price;
-	private ArrayList<UserEntry> users;
-	
-	public Service(String name, ArrayList<UserEntry> users){
+	private String					name;
+	private long					time;
+	private double					price;
+	private ArrayList<UserEntry>	users;
+
+	public Service(String name, ArrayList<UserEntry> users) {
 		this.name = name;
 		this.users = users;
 		this.time = 0;
 		this.price = 0;
 	}
-	
-	public Service(String serviceName){
+
+	public Service(String serviceName) {
 		this(serviceName, null);
 	}
-	
-	public void addUserEntry(UserEntry user){
-		if(users == null){
+
+	public void addUserEntry(UserEntry user) {
+		if (users == null) {
 			users = new ArrayList<UserEntry>();
 		}
-		
+
 		users.add(user);
 	}
 
@@ -64,13 +68,45 @@ public class Service {
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
-	@Override
-	public boolean equals(Object arg0) {
-		if(!(arg0 instanceof Service)){
-			return false;
+
+	public ArrayList<ArrayList<Object>> getAsTable() {
+		ArrayList<ArrayList<Object>> data = new ArrayList<ArrayList<Object>>();
+		ArrayList<Object> row;
+		Boolean first = true;
+
+		if (users == null) {
+			/**
+			 * Columns from row : Service Name, Status, User, Offer made, Time,
+			 * Price
+			 */
+			row = new ArrayList<Object>(Arrays.asList(getName(), "Inactive", "", "", "", ""));
+			data.add(row);
+		} else {
+			for (UserEntry user : users) {
+				row = new ArrayList<Object>();
+				if (first) {
+					first = false;
+
+					row = new ArrayList<Object>(Arrays.asList(getName(), "Active", user.getName(),
+							user.getOffer(), user.getTime(), user.getPrice()));
+				} else {
+					row = new ArrayList<Object>(Arrays.asList("", "", user.getName(),
+							user.getOffer(), user.getTime(), user.getPrice()));
+				}
+
+				data.add(row);
+			}
 		}
 		
-		return ((Service)arg0).getName().equals(name);
+		return data;
+	}
+
+	@Override
+	public boolean equals(Object arg0) {
+		if (!(arg0 instanceof Service)) {
+			return false;
+		}
+
+		return ((Service) arg0).getName().equals(name);
 	}
 }
