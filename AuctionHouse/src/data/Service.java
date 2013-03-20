cbb1ae6a8bc3a5b17ea8fdec19832ac6a72a67ce
Paul Vlase <vlase.paul@gmail.com>
2013-03-20 15:41:09 +0200

    data: New data members for Service class

--- AuctionHouse/src/data/Service.java
+++ AuctionHouse/src/data/Service.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 
 /**
  * If user field is null, then this offer is inactive, otherwise it's active.
+ * If time is 0, this offer is expired.
+ * If price is 0, this offer is a demand, otherwise it's supply.
  * @author Ghennadi Procopciuc
  */
 public class Service {
 	private String serviceName;
+	private long time;
+	private double price;
 	private ArrayList<UserEntry> users;
 	
 	public Service(String serviceName, ArrayList<UserEntry> users){
 		this.serviceName = serviceName;
 		this.users = users;
+		this.time = 0;
+		this.price = 0;
 	}
 	
 	public Service(String serviceName){
@@ -41,5 +47,21 @@ public class Service {
 
 	public void setServiceName(String serviceName) {
 		this.serviceName = serviceName;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long time) {
+		this.time = time;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
 	}
 }
