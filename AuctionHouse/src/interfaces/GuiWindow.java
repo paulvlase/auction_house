688f73b54540b5d726c1c7da34da130a41ebf4c5
Paul Vlase <vlase.paul@gmail.com>
2013-03-21 19:20:25 +0200

    interfaces: New interface for Gui.

---
+++ AuctionHouse/src/interfaces/GuiWindow.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+import data.LoginCred;
+import data.UserProfile;
+
+public interface GuiWindow {
+	public void logIn(LoginCred cred);
+	public void logOut();
+	public UserProfile getUserProfile();
+}
