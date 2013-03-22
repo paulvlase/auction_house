1629abf7b0a6187206128fab606426a7252f7a3e
Ghennadi Procopciuc <unix140@gmail.com>
2013-03-22 20:50:29 +0200

    mediator: New data.

--- AuctionHouse/src/mediator/MockupMediator.java
+++ AuctionHouse/src/mediator/MockupMediator.java
@@ -11,7 +11,10 @@ import config.GlobalConfig.ServiceType;
 import config.GlobalConfig.UserType;
 import data.LoginCred;
 import data.Service;
+import data.UserEntry;
 import data.UserProfile;
+import data.Service.Status;
+import data.UserEntry.Offer;
 import data.UserProfile.UserRole;
 import interfaces.Gui;
 import interfaces.MediatorGui;
@@ -137,16 +140,34 @@ public class MockupMediator implements MediatorGui, MediatorNetwork, MediatorWeb
 	
 	@Override
 	public ArrayList<Service> loadOffers() {
-		if (profile == null)
-			return null;
-		
-		ArrayList<Service> services = null;
-		if (profile.getRole() == UserRole.BUYER)
-			services = loadServicesFile(FilesConfig.DEMANDS_FILENAME, ServiceType.DEMAND);
+//		if (profile == null)
+//			return null;
+//		
+//		ArrayList<Service> services = null;
+//		if (profile.getRole() == UserRole.BUYER)
+//			services = loadServicesFile(FilesConfig.DEMANDS_FILENAME, ServiceType.DEMAND);
+//
+//		if (profile.getRole() == UserRole.SELLER)
+//			services = loadServicesFile(FilesConfig.SUPPLIES_FILENAME, ServiceType.SUPPLY);
+//		
+//		return services;
+		ArrayList<Service> services = new ArrayList<Service>();
+		for (int i = 0; i < 10; i++) {
 
-		if (profile.getRole() == UserRole.SELLER)
-			services = loadServicesFile(FilesConfig.SUPPLIES_FILENAME, ServiceType.SUPPLY);
-		
+			Service service1 = new Service("service1");
+			Service service2 = new Service("service2", Status.ACTIVE);
+			Service service3 = new Service("service3", Status.TRANSFER_STARTED);
+
+			service2.addUserEntry(new UserEntry("Paul Vlase", Offer.NO_OFFER, 100L, 25.2));
+			service2.addUserEntry(new UserEntry("Ghennadi", Offer.NO_OFFER, 101L, 28.7));
+			service2.addUserEntry(new UserEntry("Ana", Offer.NO_OFFER, 102L, 29.9));
+
+			service3.addUserEntry(new UserEntry("Paul Vlase", Offer.OFFER_ACCEPTED, 100L, 25.2));
+
+			services.add(service1);
+			services.add(service2);
+			services.add(service3);
+		}
 		return services;
 	}
 	
