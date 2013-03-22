224a6436366003b21240892debe727d74250519d
Paul Vlase <vlase.paul@gmail.com>
2013-03-22 18:16:35 +0200

    config: Nothing here.

--- AuctionHouse/src/config/FilesConfig.java
+++ AuctionHouse/src/config/FilesConfig.java
@@ -6,8 +6,8 @@ package config;
  * @author Paul Vlase <vlase.paul@gmail.com>
  */
 public class FilesConfig {
-	public static String LOGIN_FILENAME = "invalidLogin.conf"; 
-	//public static String LOGIN_FILENAME = "login.conf";
+	//public static String LOGIN_FILENAME = "invalidLogin.conf"; 
+	public static String LOGIN_FILENAME = "login.conf";
 	public static String DEMANDS_FILENAME = "demands.list";
 	public static String SUPPLIES_FILENAME = "supplies.list";
 }
