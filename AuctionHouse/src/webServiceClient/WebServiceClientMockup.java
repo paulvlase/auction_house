f1c432235df61a4b17efe280a9f193b78cf5f6c3
Paul Vlase <vlase.paul@gmail.com>
2013-03-23 12:20:12 +0200

    webServiceClient: [WebServiceClient] Rename webServiceClient

---
+++ AuctionHouse/src/webServiceClient/WebServiceClientMockup.java
@@ -0,0 +1,128 @@
+package webServiceClient;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import data.LoginCred;
+import data.Service;
+import data.UserProfile;
+import data.UserProfile.UserRole;
+import interfaces.MediatorWeb;
+import interfaces.WebServiceClient;
+
+/**
+ * WebServiceClient module implementation.
+ * 
+ * @author Paul Vlase <vlase.paul@gmail.com>
+ */
+public class WebServiceClientMockup implements WebServiceClient {
+	private MediatorWeb med;
+	private WebServiceMockup webService;
+	
+	private Hashtable<String, UserProfile> users;
+	
+	public WebServiceClientMockup(MediatorWeb med) {
+		this.med = med;
+		
+		med.registerWebServiceClient(this);
+
+		/* TODO: This should be deleted.
+		 * Used only for mockup test.
+		 */
+		users = new Hashtable<String, UserProfile>();
+		users.put("pvlase", new UserProfile("pvlase","Paul",  "Vlase", UserRole.BUYER, "parola"));
+		users.put("unix140", new UserProfile("unix140","Ghennadi",  "Procopciuc", UserRole.BUYER, "marmota"));
+	}
+	
+	@Override
+	public UserProfile logIn(LoginCred cred) {
+		UserProfile profile;
+		
+		profile = getUserProfile(cred.getUsername());
+		if (profile == null) {
+			return null;
+		}
+		
+		if (!profile.getPassword().equals(cred.getPassword())) {
+			return null;
+		}
+		
+		return profile;
+	}
+	
+	@Override
+	public void logOut() {
+		System.out.println("[WebServiceClient:logOut()] Bye bye");
+		
+		/* TODO: This should be deleted.
+		 * Used only for mockup test.
+		 */
+		try {
+			webService.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public UserProfile getUserProfile(String username) {
+		return users.get(username);
+	}
+	
+	@Override
+	public boolean setUserProfile(UserProfile profile) {
+		users.put(profile.getUsername(), profile);
+		return true;
+	}
+
+	/* Common */
+	@Override
+	public boolean launchOffer(Service service) {
+		
+		return false;
+	}
+	
+	@Override
+	public boolean launchOffers(ArrayList<Service> service) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean dropOffer(Service service) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public boolean dropOffers(ArrayList<Service> service) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* Buyer */
+	@Override
+	public int acceptOffer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int refuseOffer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* Seller */
+	@Override
+	public int makeOffer() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int dropAction() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
