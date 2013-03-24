1c65fbc3145e90f5c000c3ea73843c261601f5af
paulvlase <vlase.paul@gmail.com>
2013-03-24 14:31:53 +0200

    network: Fix missing files.

---
+++ AuctionHouse/src/network/NetworkJoinThread.java
@@ -0,0 +1,57 @@
+package network;
+
+import interfaces.MediatorNetwork;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import javax.swing.SwingWorker;
+
+import data.Service;
+
+public class NetworkJoinThread extends Thread {
+	MediatorNetwork med;
+	Hashtable<String, NetworkTask> tasks;
+	Object monitor;
+
+	public NetworkJoinThread(MediatorNetwork med) {
+		this.med = med;
+		tasks = new Hashtable<String, NetworkTask>();
+		monitor = new Object();
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				synchronized(monitor) {
+					monitor.wait();
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Cine m-a trezit baaa?");
+			makeJoin();
+		}
+	}
+	
+	private synchronized void makeJoin() {
+		for (Map.Entry<String, NetworkTask> entry: tasks.entrySet()) {
+			NetworkTask task = entry.getValue();
+			try {
+				task.get();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		tasks.clear();
+	}
+	
+	public synchronized void registerJoin(NetworkTask task) {
+		tasks.put(task.getServiceName(), task);
+	}
+	
+	public Object getMonitor() {
+		return monitor;
+	}
+}
