9c9cbe6f8c593c03eb9b114f169c33db2b2ffbec
paulvlase <vlase.paul@gmail.com>
2013-04-11 21:55:51 +0300

    config: Update WebServiceClientConfig.

--- AuctionHouse/src/config/WebServiceClientConfig.java
+++ AuctionHouse/src/config/WebServiceClientConfig.java
@@ -6,5 +6,6 @@ package config;
  * @author Paul Vlase <vlase.paul@gmail.com>
  */
 public class WebServiceClientConfig {
-	
+	public static final String	IP		= "127.0.0.1";
+	public static final int		PORT	= 3333;
 }
