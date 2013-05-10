d56cbe11646a2bbd84029e791a17f3fd992aeffb
paulvlase <vlase.paul@gmail.com>
2013-05-10 22:21:21 +0300

    webServer: I missed magic commits :D.

--- AuctionHouseWebServer/src/webServer/WebServer.java
+++ AuctionHouseWebServer/src/webServer/WebServer.java
@@ -3,7 +3,16 @@ package webServer;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+
+import mediator.MediatorImpl;
+
+import org.apache.log4j.Logger;
 
 import webServer.messages.DropOfferRequest;
 import webServer.messages.GetProfileRequest;
@@ -24,15 +33,80 @@ import data.UserProfile;
 import data.UserProfile.UserRole;
 
 public class WebServer {
+	static String			name	= "root";
+	static String			pass	= "student";
+	static Connection		conn;
+
+	static {
+		System.out.println("[WebServer:static] Begin");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		String url = "jdbc:mysql://127.0.0.1:3306/auction_house";
+
+		try {
+			conn = DriverManager.getConnection(url, name, pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("[WebServer:static] End");
+	}
 
 	// public Object login(LoginRequest req) {
-	// return "";
-	// }
-	//
+	public Object login(Object req) {
+		System.out.println("[WebServer:login] Begin");
+
+		String username = "pvlase";
+		String password = "parola";
+
+		Statement simpleStatement;
+		try {
+			simpleStatement = conn.createStatement();
+			
+			String query = 
+					"SELECT *" +
+					" FROM users" +
+					" WHERE username = " + username +
+					" AND password = " + password;
+			ResultSet rs = simpleStatement.executeQuery(query);
+
+			if (rs.first()) {
+				Integer id = 1;
+				System.out.println("[WebServer:login] Logged in");
+				query = 
+						"UPDATE users" +
+						" SET as_seller = " + 1 +
+						" WHERE id = " + id;
+
+				simpleStatement.executeUpdate(query);
+			} else {
+				System.out.println("[WebServer:login] Error to logged in");
+			}
+			/*
+			while (rs.next()) {
+				String username = rs.getString("username");
+				String password = rs.getString("password");
+
+				System.out.println("[WebServer:login] username: " + username + " password: " + password);
+			}
+			*/
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("[WebServer:login] End");
+		return "";
+	}
+
 	// public Object logout(LogoutRequest requestMsg) {
-	// return "";
-	// }
-	//
+	public Object logout(Object requestMsg) {
+		
+		return "";
+	}
+
 	// public Object launchOffer(LaunchOfferRequest req) {
 	// return "";
 	// }
@@ -52,7 +126,7 @@ public class WebServer {
 	// public Object registerProfile(RegisterProfileRequest req) {
 	// return "";
 	// }
-
+	
 	public float celsiusToFarenheit(float celsius) {
 		return (celsius * 9 / 5) + 32;
 	}
@@ -64,6 +138,4 @@ public class WebServer {
 	public Object farenheitToCelsius1(Object farenheit) {
 		return 1;
 	}
-	
-	
 }
