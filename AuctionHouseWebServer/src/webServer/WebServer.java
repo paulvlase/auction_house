6fab565e9c9b4b712cd3a4e0ea157d49c6ea1f8c
paulvlase <vlase.paul@gmail.com>
2013-05-11 14:09:58 +0300

    webServer: Fix imports.

--- AuctionHouseWebServer/src/webServer/WebServer.java
+++ AuctionHouseWebServer/src/webServer/WebServer.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
+import webClient.WebMessage;
 import webServer.messages.DropOfferRequest;
 import webServer.messages.ErrorResponse;
 import webServer.messages.GetProfileRequest;
