7d3c930f2da21988a8f294e3b4e116d9050dde1b
paulvlase <vlase.paul@gmail.com>
2013-05-11 20:41:10 +0300

    web.messages: No changes after this point

--- AuctionHouseWebServer/src/webServer/messages/LaunchOfferRequest.java
+++ AuctionHouseWebServer/src/webServer/messages/LaunchOfferRequest.java
@@ -4,25 +4,18 @@ import java.io.Serializable;
 
 import data.LoginCred;
 import data.Service;
-import data.UserProfile.UserRole;
 
 public class LaunchOfferRequest implements Serializable {
 	private static final long	serialVersionUID	= 1L;
-	public String username;
 	public Service service;
 	public LoginCred cred;
 	
-	public LaunchOfferRequest(String username, LoginCred cred, Service service) {
-		this.username = username;
+	public LaunchOfferRequest(LoginCred cred, Service service) {
 		this.cred = cred;
 		this.service = service;
 	}
 	
-	public String getUsername() {
-		return username;
-	}
-	
-	public LoginCred getCred() {
+	public LoginCred getLoginCred() {
 		return cred;
 	}
 	
