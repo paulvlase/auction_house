7476ea78ef36c4dde9449dfdeeeea3376df89b2c
Ghennadi Procopciuc <unix140@gmail.com>
2013-05-11 14:40:25 +0300

    webServer: Add constants for DB connection.

--- AuctionHouseWebServer/src/config/WebServerConfig.java
+++ AuctionHouseWebServer/src/config/WebServerConfig.java
@@ -2,4 +2,7 @@ package config;
 
 public class WebServerConfig {
 	public static String DB_SERVER = "127.0.0.1";
+	public static String DB_NAME = "";
+	public static String DB_USER = "";
+	public static String DB_PASSWORD = "";
 }
